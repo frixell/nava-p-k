cb0cbdd782b5e76ef87a911c4bd5db005a46a522
mosh kainer <mosh.kainer@comm-it.com>
2020-04-17 23:50:44 +0300

    first map 45

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -12,7 +12,7 @@ class Footer extends React.Component {
         const dirLang = this.props.lang === 'he' ? 'rtl' : 'ltr';
         return (
 
-            <div className="footer__box">
+            <div className={`footer__box${this.props.lang === 'en' ? ' footer__box--en' : ' footer__box--he'}`}>
                 {/*<HomePageIntouch />*/}
                 <p className="footer__text Heebo-Regular desktop" dir={dirLang}>
                     {this.context.t("allRightsReserved")} | {this.context.t("programmingBy")}
